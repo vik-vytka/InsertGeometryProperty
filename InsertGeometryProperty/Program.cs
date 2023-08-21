61e859de4d32d1a2125a72166590041cd8442623
Vyacheslav Lozhkin <vik-vytka@yandex.ru>
2023-08-21 23:03:28 +0300

    initial module correct the files gml for fgis

---
+++ InsertGeometryProperty/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+
+namespace InsertGeometryProperty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            ChangeXML(path);
+            Console.WriteLine("Готово");
+            Console.ReadKey();
+        }
+        
+        static void ChangeXML(string path)
+        {
+            List<string> names = new List<string>() { "Point", "LineString", "Polygon" };
+            XDocument doc = XDocument.Load(path);
+            XElement root = doc.Root;
+            XNamespace space = root.Name.Namespace;
+            var features = root.Elements(space.GetName("featureMember"));
+            foreach(XElement feature in features)
+            {
+                foreach (XElement obj in feature.Elements())
+                {
+                    foreach (string name in names)
+                    {
+                        XElement el = obj.Element(space + name);
+                        if (el != null)
+                        {
+                            XElement pr = new XElement(space.GetName("geometryProperty"));
+                            pr.Add(el);
+                            el.Remove();
+                            obj.Add(pr);
+                        }
+                    }
+                }
+            }
+            string newPath = Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path) + "1.gml");
+            doc.Save(newPath);
+        }
+    }
+}
